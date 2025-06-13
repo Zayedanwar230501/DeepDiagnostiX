b2cece52396effee4891b414299c675ebdc750f1
poulami1111 <poulami.das7604@gmail.com>
2025-06-13 14:15:14 +0530

    fix:issues

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
 # Optionally restrict to certain origins (safer for production):
-# CORS(app, resources={r"/predict": {"origins": "http://localhost:3000"}})
+# CORS(app, ggg resources={r"/predict": {"origins": "http://localhost:3000"}})
 
 # Load models
 xray_model = XrayClassifierModel(
